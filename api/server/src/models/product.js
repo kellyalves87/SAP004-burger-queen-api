11175a65269551700a4679ecf9b86e67675d79ed
Kelly Alves <kellyp.alves87@gmail.com>
2020-08-25 16:52:01 -0300

    first commit

---
+++ api/server/src/models/product.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    name: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    has_extra: DataTypes.BOOLEAN,
+    is_burger: DataTypes.BOOLEAN
+  }, {});
+  Product.associate = function(models) {
+    // associations can be defined here
+  };
+  return Product;
+};
